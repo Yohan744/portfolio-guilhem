a45f324858345576366305ec763043fd30855bf1
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2021-05-05 08:41:37 +0200

    fix welcome image

--- public/js/home.js
+++ public/js/home.js
@@ -189,7 +189,7 @@ const requestProjects = async () => {
                 }
             }
 
-            let verifWelcome = data.title.indexOf("Bienvenu !")
+            let verifWelcome = data.title.indexOf("bienvenu !")
             if ((verifWelcome !== -1) && (verifWelcome < 30)) {
                 welcomeImageWrapper.innerHTML = `<img src="${data.images.hidpi}" alt="welcome" class="image">`
                 welcomeTitleWrapper.innerHTML = `<h1 class="welcome-left-title">${data.title}</h1>`
